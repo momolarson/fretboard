e8de24190c3061b1f582bac34e3779e6466b5315
Gregg Larson <larsongb@gmail.com>
2020-12-24 08:15:26 -0700

    added react app example

---
+++ example/src/App.js
@@ -0,0 +1,20 @@
+import logo from './gtc_verylarge_circular.png';
+import './App.css';
+import Fretboard from './components/Fretboard';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Fretboard Diagram Generator presented by <a href="https://www.guitartabcreator.com" className="App-link">Guitar Tab Creator</a>
+        </p>
+      </header>
+      <Fretboard />
+    </div>
+  );
+}
+
+export default App;
