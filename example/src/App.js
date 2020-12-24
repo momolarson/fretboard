41700ec302d2a1775cc395a38506d4d9f633bca9
Gregg Larson <larsongb@gmail.com>
2020-12-24 09:43:21 -0700

    minor stuff

--- example/src/App.js
+++ example/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <a href="https://www.guitartabcreator.com"><img src={logo} className="App-logo" alt="logo" /></a>
         <p>
         Fretboard Diagram Generator presented by <a href="https://www.guitartabcreator.com" className="App-link">Guitar Tab Creator</a>
         </p>
